ec91b448c98b42cd5ef86deffeb4b2efb22715f6
Ahmed Raza <hafizahmedmoon@gmail.com>
2025-01-13 19:22:35 +0500

    add firebase

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+} from '@ionic/angular/standalone';
+import { DocumentData, FirebaseFirestore } from '@capacitor-firebase/firestore';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,10 +15,44 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonItem,
+    IonList,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab2Page {
+  users = signal<DocumentData[]>([]);
 
-  constructor() {}
+  constructor() {
+    this.loadUsers();
+  }
 
+  async loadUsers() {
+    const id = await FirebaseFirestore.addCollectionSnapshotListener(
+      {
+        reference: 'users',
+      },
+      (event, error) => {
+        if (event != null) {
+          this.users.set(event.snapshots);
+        }
+      }
+    );
+
+  }
+
+  updateName(id: string, text: string) {
+
+    FirebaseFirestore.updateDocument({
+      reference: `users/${id}`,
+      data: {
+        name: text
+      }
+    })
+  }
 }
